860aafded1f5b0275f22cd69b7fed6611fabf152
Derek Lim <dereklim3006@gmail.com>
2023-06-16 20:22:04 -0400

    Add button to prompt user for grid size

--- javascript.js
+++ javascript.js
@@ -1,24 +1,42 @@
 const body = document.querySelector('body');
-const container = document.querySelector('div');
+const container = document.querySelector('.container');
+const buttonContainer = document.querySelector('#buttonContainer')
+const button = document.createElement('button');
+buttonContainer.append(button);
+button.textContent = "Click to set grid size (max: 100)";
 
-//create 16x16 grid of square divs
-const row = [];
-for (i = 0; i < 16; i++) {
-    row[i] = document.createElement('div');
-    row[i].classList.add('row');
-    row[i].setAttribute('style', 'display: flex; justify-content: center;');
-    container.append(row[i]);
-    const div = [];
-    for (j = 0; j < 16; j++) {
-        div[j] = document.createElement('div');
-        div[j].classList.add('column');
-        div[j].setAttribute('style', 'height: 45px; width: 45px; border: solid black')
-        row[i].append(div[j]);
+//create function that asks user for number of squares per side of grid
+let getGridSize = function() {
+    gridSize = +prompt('Size: ');
+    while (gridSize <= 0 || gridSize > 100) {
+        gridSize = prompt('Please enter a valid number');
     }
-} 
+    return gridSize;
+}
 
-//make divs toggle between black and white as mouse hovers over
-const divs = document.querySelectorAll('.column')
-divs.forEach((div) => {
-    div.addEventListener('mouseover', () => {div.classList.toggle('black');});
-})
+// make button prompt user when clicked
+button.addEventListener('click', () => {
+    getGridSize();
+
+    //create 16x16 grid of square divs
+    const row = [];
+    for (i = 0; i < gridSize; i++) {
+        row[i] = document.createElement('div');
+        row[i].classList.add('row');
+        row[i].setAttribute('style', 'display: flex; justify-content: center;');
+        container.append(row[i]);
+        const div = [];
+        for (j = 0; j < gridSize; j++) {
+            div[j] = document.createElement('div');
+            div[j].classList.add('square');
+            div[j].setAttribute('style', 'height: 45px; width: 45px; border: solid black')
+            row[i].append(div[j]);
+        }
+    } 
+
+    //make divs toggle between black and white as mouse hovers over
+    const divs = document.querySelectorAll('.square')
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {div.classList.toggle('black');});
+    })
+});
